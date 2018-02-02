b29e3217326c5a635700ebfae0f450fc1595513f
LeslyN <lnombertoc@gmail.com>
2018-02-01 21:08:42 -0500

    estilos de figcaption, modificación de mouseout, validación

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
 window.addEventListener('load', function () {
-  $('.materialboxed').materialbox();
   $('img').cardify();
 });
--- public/js/index.js
+++ public/js/index.js
@@ -5,20 +5,36 @@
 (function (obj) {
   $.fn.cardify = function () {
     var $listOfImages = $('img');
+    var $containerImages = $('container');
     $listOfImages.each(function () {
       var _this = this;
 
-      $(this).one('mouseover', function () {
-        // $(this).addClass('opacity');
-        var $textAlt = $(_this).attr('alt');
-        var $figure = $('<figure></figure>');
-        $(_this).wrap($figure).parent();
-        var $figcaption = $('<figcaption></figcaption>');
-        $figcaption.addClass('figcaption-position');
-        $(_this).parent().append($figcaption);
-        console.log($(_this).next().text($textAlt));
+      // Evento que se ejecuta cuando el mouse está encima de la imagen
+      if ($containerImages) {
+        $(this).on('mouseover', function () {
+          var $textAlt = $(_this).attr('alt');
+          var $figure = $('<figure></figure>');
+          $(_this).wrap($figure).parent();
+          var $figcaption = $('<figcaption></figcaption>');
+          $figcaption.addClass('figcaption-position');
+          $(_this).parent().append($figcaption);
+          $figcaption.show();
+          console.log($(_this).next().text($textAlt));
+        });
+      }
+    });
+
+    $(this).each(function () {
+      var _this2 = this;
+
+      // Evento que se ejecuta cuando el mouse deja de estar encima de la imagen
+      var $mouseout = $(this).on('mouseout', function () {
+        $(_this2).unwrap('figure');
+        $(_this2).next().hide();
+        // alert('This is finished!!!!');
       });
     });
+
     return $(this);
   };
 })(jQuery);
