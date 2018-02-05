c309eeec2abcd2f507f1ed706200924e0609f883
AraceliGS <aragutarras@gmail.com>
2018-02-04 20:56:01 -0500

    modificando errores segun la guia de estilos

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,20 @@
 
 // Cardify
 // Función autoejecutable
-(function (obj) {
-  $.fn.cardify = function () {
-    $(".button-collapse").sideNav();
+(function(obj) {
+  $.fn.cardify = function() {
+    $('.button-collapse').sideNav();
     var $listOfImages = $('img');
     var $containerImages = $('container');
     // $listOfImages.each(function() {
     // Evento que se ejecuta cuando el mouse está encima de la imagen
     if ($containerImages) {
       $listOfImages.wrap('<figure></figure>');
-      $('figure').hover(function () {
+      $('figure').hover(function() {
         var $textAlt = $(this).children().attr('alt');
         $(this).append('<figcaption>' + $textAlt + '</figcaption></figure>');
         $('figcaption').addClass('figcaption-position');
-      }, function () {
+      }, function() {
         $('figcaption').remove();
       });
     } else {
